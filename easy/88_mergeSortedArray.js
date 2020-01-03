aca06e56678b62463a57d1967e4fd81640d14711
sudoxx2 <sudoxx2@gmail.com>
2020-01-03 12:49:12 -0800

    Update 88_mergeSortedArray.js
    There was a stipulation where we needed to manipulate the already given array so creating a new array wouldn't work.

--- easy/88_mergeSortedArray.js
+++ easy/88_mergeSortedArray.js
@@ -6,42 +6,25 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1 = nums1.splice(0,3)
-    
-    var sortedMerge = []
-    
+
     let i = 0, j = 0
     
-    while(nums1.length != i && nums2.length != j) {
-        if(nums1[i] < nums2[j]) {
-            sortedMerge.push(nums1[i])
+    nums1.splice(m, nums1.length)
+    
+    while(m+n != i && m+n != j) {
+        // case where end of first array but second array has finished scanning
+        if(nums1.length <= i && nums2.length != j) {
+            nums1[i] = nums2[j]
+            j++
             i++
         } else if(nums1[i] > nums2[j]) {
-            sortedMerge.push(nums2[j])
+            nums1.splice(i, 0, nums2[j])
             j++
+            i+2
+        } else if(nums1[i] < nums2[j]) {
+            i++ 
         } else {
-            sortedMerge.push(nums1[i])
-            sortedMerge.push(nums2[j])
             i++
-            j++
         }
     }
-    
-    if(i > j) {
-        sortedMerge = sortedMerge.concat(nums2.splice(j, nums2.length))
-    }
-    
-    if(i < j){
-        sortedMerge = sortedMerge.concat(nums1.splice(i, nums1.length))
-    }
-    
-    console.log(sortedMerge.length)
-    console.log(sortedMerge)
-    
-    
-    nums1 = sortedMerge
-    
-    nums1 = nums1.push(4)
-    
-    console.log(nums1)
 };
