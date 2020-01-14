a9a783282fce2885ce598f29cba36d76aa8c2b4f
sudoxx2 <sudoxx2@gmail.com>
2020-01-14 01:52:23 -0800

    Update 238_productOfArrayExceptSelf.js

--- med/238_productOfArrayExceptSelf.js
+++ med/238_productOfArrayExceptSelf.js
@@ -25,3 +25,34 @@ var productExceptSelf = function(nums) {
     
 };
 
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Accepted answer
+ */
+var productExceptSelf = function(nums) {
+    let left = [1], right = [1], ansArr = []
+    
+    
+    let prod = 0
+    for(let i = 0; i < nums.length - 1; i++) {
+        prod = left[i] * nums[i]
+        left.push(prod)
+    }
+    
+    prod = 0
+    
+    for(let i = nums.length - 1, j = 0; i > 0; i--, j++) {
+        prod = right[j] * nums[i]
+        right.push(prod)
+    }
+    
+    right = right.reverse()
+    
+    for(let i = 0; i < nums.length; i++) {
+        ansArr.push(left[i] * right[i])
+    }
+    
+    return ansArr
+};
